45c44d573de9dfbfd328bae7922dba44c7b99177
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-03-26 04:16:58 -0500

    Fix critical bug in auth middleware function

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 
 module.exports = {
     // function for our authenticated routes
-    authMiddleware: function ({ req }) {
+    authMiddleware: function ( req, res, next ) {
         // allows token to be sent via  req.query or headers
         let token = req.query.token || req.headers.authorization || req.body.token;
 
@@ -16,8 +16,9 @@ module.exports = {
         }
 
         if (!token) {
-            return req;
+            return res.status(400).json({ message: 'You have no token!' })
         }
+
 
         // verify token and get user data out of it
         try {
@@ -25,10 +26,11 @@ module.exports = {
             req.user = data;
         } catch {
             console.log('Invalid token');
+            return res.status(400).json({ message: 'invalid token!' });
         }
 
         // send to next endpoint
-        return req;
+        next();
     },
     signToken: function ({ username, email, _id }) {
         const payload = { username, email, _id };
