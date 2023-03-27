3b343032f79d2a5bcbd5919ed27cd792d39e6e1a
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-03-27 01:13:25 -0500

    Set up layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import SignupForm from './components/SignupForm.js';
+import Layout from './components/Layout.js';
+import React from "react";
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
-      <h1 className="text-blue-600">barkr</h1>
+    <div className="flex flex-col h-screen">
+      <Router>
+        <Layout >
+          <SignupForm />
+        </Layout>
+      </Router>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
