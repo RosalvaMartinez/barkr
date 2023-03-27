bca315c09cbad79510eb8cd0a616d4ff4754d186
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-03-27 01:12:31 -0500

    Create footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className=" bg-slate-400 flex flex-row content-center w-full mt-auto bg-secondary p-4">
+            <div className="flex flex-col justify-center place-items-center my-5 w-full">
+                <h3>
+                    Thanks for using Barkr, your pets will thank you!
+                </h3>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
