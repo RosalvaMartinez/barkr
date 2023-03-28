b1ee14352933725c22e0090878da908647190d2f
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-03-28 14:43:09 -0500

    Refactor Login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,26 +1,68 @@
+import { useState } from 'react';
+import Auth from '../utils/auth.js';
+
 function LoginForm() {
+   
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [emailError, setEmailError] = useState(false)
+    const [passwordError, setPasswordError] = useState(false)
+    const [emailErrorMessage, setEmailErrorMessage] = useState("")
+    const [passwordErrorMessage, setPasswordErrorMessage] = useState("")
+
+    const handleFormsubmit = async (e) => {
+        e.preventDefault()
+        if (emailError || passwordError) return
+        const res = await fetch('/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        })
+        const { token, user } = await res.json()
+        Auth.login(token)
+    
+    }
+    
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+        if (e.target.value === "") {
+            setEmailError(true)
+            setEmailErrorMessage("must enter a valid email")
+        } else {
+            setEmailError(false)
+            setEmailErrorMessage("")
+        }
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+        if (e.target.value === "") {
+            setPasswordError(true)
+            setPasswordErrorMessage("must enter a password")
+        } else {
+            setPasswordError(false)
+            setPasswordErrorMessage("")
+        }
+    }
+
     return (
         <div className="flex flex-col w-full h-full justify-center place-items-center">
-            <form className="flex flex-col border border-black bg-slate-900 rounded-xl w-fit p-4 justify-center">
-                <label className="block">
-                    <span className="block text-lg font-medium text-slate-300">Username:</span>
-                    <input type="username" className=" rounded peer ..." />
+            <form onSubmit={handleFormsubmit} className="flex flex-col border border-black bg-slate-500 rounded-xl w-[300px] p-4 justify-center">
+                <label className="my-4">
+                    <input className="rounded w-full p-2" placeholder="email" name="email" type="text" value={email} onChange={handleEmailChange} />
+                    <div className={`${emailError ? "visible" : "invisible"} h-4 text-red-600`}>{emailErrorMessage}</div>
                 </label>
-                <label className="block">
-                    <span className="block text-lg font-medium text-slate-300">Email:</span>
-                    <input type="email" className="rounded peer ..." />
-                    <p className="mt-2 invisible peer-invalid:visible text-red-600 text-md">
-                    insert something here.
-                    </p>
+                <label className="my-4">
+                    <input className="rounded w-full p-2" placeholder="password" name="password" type="text" value={password} onChange={handlePasswordChange} />
+                    <div className={`${passwordError ? "visible" : "invisible"} h-4 text-red-600`}>{passwordErrorMessage}</div>
                 </label>
-                <label className="block">
-                    <span className="block text-lg font-medium text-slate-300">Password:</span>
-                    <input type="email" className="rounded peer ..." />
-                    <p className="mt-2 invisible peer-invalid:visible text-red-600 text-md">
-                        insert something here.
-                    </p>
-                </label>
-                <button className="font-sans font-normal bg-blue-300 hover:bg-blue-500 active:bg-blue-400 p-2 rounded-lg text-lg focus:outline-none focus:ring focus:ring-blue-200 ...">
+                <button type="submit" className="font-sans font-normal bg-blue-300 hover:bg-blue-500 active:bg-blue-400 p-2 rounded-lg text-lg focus:outline-none focus:ring focus:ring-blue-200">
                     Login
                 </button>
             </form>
