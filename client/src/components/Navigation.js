cd7676679311384dda57cc21625aa4c1c3d932db
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-03-27 23:07:39 -0500

    Refactor navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -4,22 +4,38 @@ import {
 } from "react-router-dom";
 
 function Navigation() {
+    const navItems = [
+        {
+            path: "/",
+            title: "Home"
+        },
+        {
+            path: "/dashboard",
+            title: "My Dashboard"
+        },
+        {
+            path: "/dogProfile",
+            title: "Dog Profile"
+        },
+        {
+            path: "/aboutUs",
+            title: "About Us"
+        },
+    ]
     return (
         <nav>
-            <ul className="flex flex-row p-6">
-                <button className="bg-slate-900 hover:bg-blue-500 active:bg-blue-400 p-2 rounded-lg text-sm text-slate-100 ...">
-                    <Link to="/">Home</Link>
-                </button>
-                <button className="bg-slate-900 hover:bg-blue-500 active:bg-blue-400 p-2 rounded-lg text-sm text-slate-100 ...">
-                    <li className="mx-2"><Link to="/dashboard">My Dashboard</Link></li>
-                </button>
-                <button className="bg-slate-900 hover:bg-blue-500 active:bg-blue-400 p-2 rounded-lg text-sm text-slate-100 ...">
-                    <li className="mx-2"><Link to="/dogProfile">Dog Profile</Link></li>
-                </button>
-                <button className="bg-slate-900 hover:bg-blue-500 active:bg-blue-400 p-2 rounded-lg text-sm text-slate-100 ...">
-                    <li className="mx-2"><Link to="/aboutUs">About Us</Link></li>
-                </button>
+            <ul className="sm:flex hidden flex-row p-6">
+                {
+                    navItems.map((item) => {
+                        return (
+                            <li key={item.title} className="bg-slate-900 hover:bg-blue-500 active:bg-blue-400 p-2 mx-1 rounded-lg text-sm text-slate-100">
+                                <Link to={item.path}>{item.title}</Link>
+                            </li>
+                        )
+                    })
+                }
             </ul>
+            <button className="text-3xl flex sm:hidden">🐶</button>
         </nav>
     );
 }
