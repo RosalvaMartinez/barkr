0679576e82ed3cecaea951d2836eaf6aba5be5c0
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-03-28 20:37:14 -0500

    Refactor navbar logout

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,8 +3,29 @@ import {
     Link
 } from "react-router-dom";
 
+import gerald from '../utils/auth.js'
+
 function Navigation() {
-    const navItems = [
+    const navloggedOut = [
+        {
+            path: "/home",
+            title: "Home"
+        },
+        {
+            path: "/about",
+            title: "About Us"
+        },
+        {
+            path: "/signup",
+            title: "Sign Up"
+        },
+        {
+            path: "/login",
+            title: "Login"
+        }
+    ]
+
+    const navloggedIn = [
         {
             path: "/home",
             title: "Home"
@@ -22,29 +43,38 @@ function Navigation() {
             title: "About Us"
         },
         {
-            path: "/signup",
-            title: "Sign Up"
-        },
-        {
-            path: "/login",
-            title: "Login"
-        },
-        {
             path: "/addDog",
             title: "Add Dog"
         }
     ]
+
     return (
         <nav>
             <ul className="sm:flex hidden flex-row p-6">
-                {
-                    navItems.map((item) => {
+                {gerald.loggedIn() ? (
+                    navloggedIn.map((item) => {
+                        return (
+                            <li key={item.title} className="bg-slate-900 hover:bg-blue-500 active:bg-blue-400 p-2 mx-1 rounded-lg text-sm text-slate-100">
+                                <Link to={item.path}>{item.title}</Link>
+                            </li>
+
+                        )
+                    })
+                ) : (
+                    navloggedOut.map((item) => {
                         return (
                             <li key={item.title} className="bg-slate-900 hover:bg-blue-500 active:bg-blue-400 p-2 mx-1 rounded-lg text-sm text-slate-100">
                                 <Link to={item.path}>{item.title}</Link>
                             </li>
                         )
                     })
+                )}
+                {gerald.loggedIn() ? (
+                    <li onClick={gerald.logout} className="bg-slate-900 hover:bg-blue-500 active:bg-blue-400 p-2 mx-1 rounded-lg text-sm text-slate-100">Log Out
+                    </li>
+                ) : (
+                    null
+                )
                 }
             </ul>
             <button className="text-3xl flex sm:hidden">🐶</button>
