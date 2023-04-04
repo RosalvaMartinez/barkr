7ae9ef7df662beee2170984d7cac0581f5d3fc08
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-04-04 05:59:01 -0500

    Add animated buttons

--- client/src/components/LearnMore.js
+++ client/src/components/LearnMore.js
@@ -1,3 +1,7 @@
+import {
+    Link
+} from "react-router-dom";
+
 function LearnMore() {
     return (
 
@@ -11,6 +15,9 @@ function LearnMore() {
                     />
                     <h1 className="text-gray-900 dark:text-white text-3xl md:text-4xl font-sans hover:italic font-extrabold mb-2">Monitoring Your Pet's Health</h1>
                     <p className="text-lg font-normal text-gray-300 mb-6">Keeping a record of your pet's health can help you keep track of any changes in their physical and mental wellbeing. This can include changes in their weight, appetite, energy levels, and behavior. By monitoring these changes, you can detect any potential health issues early on and seek veterinary care before they become more serious.</p>
+                    <Link to="/signup" className="animate-bounce inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center font-sans text-white bg-blue-500 hover:bg-blue-400 active:bg-blue-300  rounded-lg focus:outline-none focus:ring focus:ring-blue-200">
+                        Get Started
+                    </Link>
                 </div>
                 <div className="grid md:grid-cols-2 gap-8">
                     <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
@@ -21,6 +28,9 @@ function LearnMore() {
                         />
                         <h2 className="text-gray-900 dark:text-white text-3xl font-sans hover:italic font-extrabold mb-2">Tracking Your Pet's Exercise</h2>
                         <p className="text-lg font-normal text-gray-300 mb-4">Regular exercise is important for your pet's overall health and wellbeing. By keeping track of your pet's exercise routine, you can ensure they are getting enough physical activity and adjust their routine if necessary. This is particularly important for pets that require more exercise, such as dogs, to prevent obesity and other health issues.</p>
+                        <Link to="/signup" className="animate-bounce inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center font-sans text-white bg-blue-500 hover:bg-blue-400 active:bg-blue-300  rounded-lg focus:outline-none focus:ring focus:ring-blue-200">
+                            Get Started
+                        </Link>
                     </div>
                     <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
                         <img
@@ -30,6 +40,9 @@ function LearnMore() {
                         />
                         <h2 className="text-gray-900 dark:text-white text-3xl font-sans hover:italic font-extrabold mb-2">Communication with Veterinarian</h2>
                         <p className="text-lg font-normal text-gray-300 mb-4">When you bring your pet to the veterinarian, having a detailed record of their health and exercise can help your veterinarian make more informed decisions about their care. It can also help them diagnose any potential health issues more quickly and accurately.</p>
+                        <Link to="/signup" className="animate-bounce inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center font-sans text-white bg-blue-500 hover:bg-blue-400 active:bg-blue-300  rounded-lg focus:outline-none focus:ring focus:ring-blue-200">
+                            Get Started
+                        </Link>
                     </div>
                 </div>
                 <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 my-8 rounded-lg p-8 md:p-12 mb-8">
@@ -40,6 +53,9 @@ function LearnMore() {
                     />
                     <h1 className="text-gray-900 dark:text-white text-3xl font-sans hover:italic  md:text-4xl font-extrabold mb-2">Peace of Mind</h1>
                     <p className="text-lg font-normal text-gray-300 mb-6">Keeping records of your pet's health and exercise can give you peace of mind, knowing that you are taking good care of your furry friend. It can also help you identify any potential issues early on, which can save you money on veterinary bills in the long run.</p>
+                    <Link to="/signup" className="animate-bounce inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center font-sans text-white bg-blue-500 hover:bg-blue-400 active:bg-blue-300  rounded-lg focus:outline-none focus:ring focus:ring-blue-200">
+                        Get Started
+                    </Link>
                 </div>
             </div>
         </div>
