96430c7149c6cf241c25ec0c50dd313170816c0b
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-04-04 03:05:06 -0500

    Update Log New Activity(DogProfile.js)

--- client/src/pages/DogProfile.js
+++ client/src/pages/DogProfile.js
@@ -1,9 +1,23 @@
-import ActivityForm from '../components/ActivityForm'
+import DietForm from '../components/DietForm'
+import StaminaForm from '../components/StaminaForm';
+import TreatsVitaminsForm from '../components/TreatsVitaminsForm';
+import MacroNutrientsForm from '../components/MacroNutrientsForm';
+import ExerciseForm from '../components/ExerciseForm';
 
 function DogProfile() {
     return (
         <>
-        <ActivityForm />
+            <div className="flex flex-col justify-center place-items-center sm:flex-row">
+                <div className="">
+                    <ExerciseForm />
+                    <MacroNutrientsForm />
+                </div>
+                <div>
+                    <StaminaForm />
+                    <DietForm />
+                    <TreatsVitaminsForm />
+                </div>
+            </div>
         </>
     )
 };
